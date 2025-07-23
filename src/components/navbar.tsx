082c57d99d33bb5093e5e8f51652b2c6d9537cdb
Isaiah <lqlp0011@gmail.com>
2025-07-23 22:21:08 +0800

    feat(comp/nav): adjust navbar alignment using page container

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,4 @@
+import { PageContainer } from "./page-container";
 import ThemeToggleButton from "./ui/theme-toggle-button";
 import { Link } from "next-view-transitions";
 
@@ -27,23 +28,25 @@ export function Navbar() {
 
   return (
     <header className="sticky top-0 z-50 bg-background/50 backdrop-blur-lg w-full">
-      <nav className="w-full max-w-3xl mx-auto px-5 py-3 flex justify-between items-center">
-        <Link
-          href="/"
-          className="text-xl font-bold dark:font-medium text-primary"
-        >
-          shenanigans.
-        </Link>
-        <div className="flex gap-5 items-center">
-          {links.length > 0 &&
-            links.map((link) => (
-              <FlatNavLink key={link.href} href={link.href}>
-                {link.text}
-              </FlatNavLink>
-            ))}
-          <ThemeToggleButton variant="circle-blur" start="top-right" />
-        </div>
-      </nav>
+      <PageContainer>
+        <nav className="w-full max-w-3xl mx-auto px-5 py-3 flex justify-between items-center">
+          <Link
+            href="/"
+            className="text-xl font-bold dark:font-medium text-primary"
+          >
+            shenanigans.
+          </Link>
+          <div className="flex gap-5 items-center">
+            {links.length > 0 &&
+              links.map((link) => (
+                <FlatNavLink key={link.href} href={link.href}>
+                  {link.text}
+                </FlatNavLink>
+              ))}
+            <ThemeToggleButton variant="circle-blur" start="top-right" />
+          </div>
+        </nav>
+      </PageContainer>
     </header>
   );
 }
