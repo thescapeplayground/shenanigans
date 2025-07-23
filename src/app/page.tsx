1fe62e3abaf6198be78113b035a245252538e330
Isaiah <lqlp0011@gmail.com>
2025-07-23 22:08:09 +0800

    feat(app/page): move projects on top of gadgets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -91,6 +91,15 @@ export default function Home() {
               </h2>
               <EducationAndLinks />
             </div>
+          </div>          <div className="w-full bg-background rounded-lg border border-border">
+            <h2 className="w-full flex items-center gap-3 text-muted-foreground px-5 py-3 border-b border-border">
+              <Server className="size-4" />
+              <span className="text-sm font-mono">projects.md</span>
+            </h2>
+            <p className="px-5 py-3 border-b border-border">
+              These are some projects that I have worked on:
+            </p>
+            <SelfHostedServices />
           </div>
           {/* Right Column */}
           <div className="space-y-6"></div>
@@ -100,16 +109,6 @@ export default function Home() {
               <span className="text-sm font-mono">gadgets.md</span>
             </h2>
             <TechGear />
-          </div>
-          <div className="w-full bg-background rounded-lg border border-border">
-            <h2 className="w-full flex items-center gap-3 text-muted-foreground px-5 py-3 border-b border-border">
-              <Server className="size-4" />
-              <span className="text-sm font-mono">projects.md</span>
-            </h2>
-            <p className="px-5 py-3 border-b border-border">
-              These are some projects that I have worked on:
-            </p>
-            <SelfHostedServices />
           </div>
         <div className="w-full bg-background rounded-lg border border-border">
           <h2 className="w-full flex items-center gap-3 text-muted-foreground px-5 py-3 border-b border-border">
