42109cb6acb394d333ccd799e99440c58f269e17
Isaiah <lqlp0011@gmail.com>
2025-07-23 22:00:12 +0800

    some corrections, dude

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,52 +67,51 @@ export default function Home() {
       </PageContainer>
       <PageContainer className="pt-4 lg:pt-8">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-        {/* Left Column */}
-        <div className="space-y-6">
-        <div className="w-full bg-background rounded-lg border border-border">
-          <h2 className="w-full flex items-center gap-3 text-muted-foreground px-5 py-3 border-b border-border">
-            <Book className="size-4" />
-            <span className="text-sm font-mono">description.md</span>
-          </h2>
-          <p className="px-5 py-3">
-            I ain&apos;t tryna be fancy here. I don&apos;t know what to put so I
-            will just say welcome to my personal site. I&apos;m Isiaih Rafael
-            Pavia. Domicile is Davao, Philippines. Age 20. For job
-            inquiries, please contact me through my work mail.
-          </p>
-          <p className="px-5 py-3">
-            Note: Placeholder, still using Lapp&apos;s description.
-          </p>
-        </div>
-        <div className="w-full bg-background rounded-lg border border-border">
-          <h2 className="w-full flex items-center gap-3 text-muted-foreground px-5 py-3 border-b border-border">
-            <GraduationCap className="size-4" />
-            <span className="text-sm font-mono">about.md</span>
-          </h2>
-          <EducationAndLinks />
-        </div>
-        </div>
-        {/* Right Column */}
-        <div className="space-y-6"></div>
-        <div className="w-full bg-background rounded-lg border border-border">
-          <h2 className="w-full flex items-center gap-3 text-muted-foreground px-5 py-3 border-b border-border">
-            <Smartphone className="size-4" />
+          {/* Left Column */}
+          <div className="space-y-6">
+            <div className="w-full bg-background rounded-lg border border-border">
+              <h2 className="w-full flex items-center gap-3 text-muted-foreground px-5 py-3 border-b border-border">
+                <Book className="size-4" />
+                <span className="text-sm font-mono">description.md</span>
+              </h2>
+              <p className="px-5 py-3">
+                I ain&apos;t tryna be fancy here. I don&apos;t know what to put so I
+                will just say welcome to my personal site. I&apos;m Isiaih Rafael
+                Pavia. Domicile is Davao, Philippines. Age 20. For job
+                inquiries, please contact me through my work mail.
+              </p>
+              <p className="px-5 py-3">
+                Note: Placeholder, still using Lapp&apos;s description.
+              </p>
+            </div>
+            <div className="w-full bg-background rounded-lg border border-border">
+              <h2 className="w-full flex items-center gap-3 text-muted-foreground px-5 py-3 border-b border-border">
+                <GraduationCap className="size-4" />
+                <span className="text-sm font-mono">about.md</span>
+              </h2>
+              <EducationAndLinks />
+            </div>
+          </div>
+          {/* Right Column */}
+          <div className="space-y-6"></div>
+          <div className="w-full bg-background rounded-lg border border-border">
+            <h2 className="w-full flex items-center gap-3 text-muted-foreground px-5 py-3 border-b border-border">
+              <Smartphone className="size-4" />
               <span className="text-sm font-mono">gadgets.md</span>
             </h2>
             <TechGear />
           </div>
-        <div className="w-full bg-background rounded-lg border border-border">
-          <h2 className="w-full flex items-center gap-3 text-muted-foreground px-5 py-3 border-b border-border">
-            <Server className="size-4" />
-            <span className="text-sm font-mono">projects.md</span>
-          </h2>
-          <p className="px-5 py-3 border-b border-border">
-            These are some projects that I have worked on:
-          </p>
-          <SelfHostedServices />
+          <div className="w-full bg-background rounded-lg border border-border">
+            <h2 className="w-full flex items-center gap-3 text-muted-foreground px-5 py-3 border-b border-border">
+              <Server className="size-4" />
+              <span className="text-sm font-mono">projects.md</span>
+            </h2>
+            <p className="px-5 py-3 border-b border-border">
+              These are some projects that I have worked on:
+            </p>
+            <SelfHostedServices />
+          </div>
         </div>
-        </div>
-        <div className="space-y-6">
         <div className="w-full bg-background rounded-lg border border-border">
           <h2 className="w-full flex items-center gap-3 text-muted-foreground px-5 py-3 border-b border-border">
             <Info className="size-4" />
@@ -176,9 +175,9 @@ export default function Home() {
             .
           </p>
         </div>
-        </div>
-        </div>
-      </PageContainer>
+      </div>
+    </div >
+    </PageContainer >
       <TextScroll
         className="text-5xl md:text-7xl text-muted-foreground/50 dark:font-semibold font-bold py-24 md:space-y-2"
         textClassName="py-1 md:py-3 font-doto"
