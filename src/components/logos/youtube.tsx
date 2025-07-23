1dc0a69d88c5be0f5be69e59962b993829a37974
Isaiah <lqlp0011@gmail.com>
2025-07-23 15:38:56 +0800

    feat(comp/logo): youtube why?

--- src/components/logos/youtube.tsx
+++ src/components/logos/youtube.tsx
@@ -2,7 +2,11 @@ export function Youtube(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg 
       xmlns="http://www.w3.org/2000/svg" 
-      viewBox="0 0 24 24" 
+      x="0px" 
+      y="0px" 
+      width="24" 
+      height="24"
+      viewBox="0 0 50 50" 
       fill="currentColor"
       {...props}
     >
