7040798270180476ac5e6661eab1597616b62c08
Isaiah <lqlp0011@gmail.com>
2025-07-23 14:41:37 +0800

    feat(app/abt): add dock icon for stuffs

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,6 +9,7 @@ import {
   HandMetal,
   ThumbsUp,
   Vote,
+  Dock
 } from "lucide-react";
 import { Metadata } from "next";
 import AwooImage from "@/assets/img/nasa.jpg";
@@ -220,7 +221,7 @@ export default function About() {
         </div>
         <div className="w-full bg-background rounded-lg border border-border">
           <h2 className="w-full flex items-center gap-3 text-muted-foreground px-5 py-3 border-b border-border">
-            <HandMetal className="size-4" />
+            <Dock className="size-4" />
             <span className="text-sm font-mono">stuffs.md</span>
           </h2>
           <p className="px-5 py-3">
